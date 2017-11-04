8c9bef7081bfeed2fa51b2f0f61ae5199b3779ae
Gary Stafford <gary.stafford@accenture.com>
2017-11-04 11:21:16 -0400

    Updating generator with latest packages and Yeoman best practices

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,69 +1,77 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var mkdirp = require('mkdirp');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const mkdirp = require('mkdirp');
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
+module.exports = class extends Generator {
+  prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
       'Welcome to the ' + chalk.red('Node-Restify-MongoDB') + ' Generator!'
     ));
-  },
-  install: function () {
+  }
+
+  install() {
     this.installDependencies({
       bower: false,
-      npm: true,
-      callback: function () {
-        console.log('npm dependencies installed!');
-      }
-    });
-  },
-  app: function () {
-    this.copy('app.js', 'app.js');
+      yarn: {force: true},
+      npm: false
+    }).then(() => console.log('npm dependencies installed!'))
+  }
+
+  app() {
+    console.log(this.projectfiles());
+    console.log(this.destinationPath());
+    this.fs.copy('app.js', 'app.js');
     mkdirp('app/models');
     mkdirp('app/routes');
-    this.copy('app/models/index.js', 'app/models/index.js');
-    this.copy('app/models/widget.js', 'app/models/widget.js');
-    this.copy('app/routes/index.js', 'app/routes/index.js');
-    this.copy('app/routes/widget.js', 'app/routes/widget.js');
-    this.copy('app/routes/utilities.js', 'app/routes/utilities.js');
-  },
-  config: function () {
+    this.fs.copy('app/models/index.js', 'app/models/index.js');
+    this.fs.copy('app/models/widget.js', 'app/models/widget.js');
+    this.fs.copy('app/routes/index.js', 'app/routes/index.js');
+    this.fs.copy('app/routes/widget.js', 'app/routes/widget.js');
+    this.fs.copy('app/routes/utilities.js', 'app/routes/utilities.js');
+  }
+
+  config() {
     mkdirp('config');
-    this.copy('config/config.js', 'config/config.js');
-  },
-  data: function () {
+    this.fs.copy('config/config.js', 'config/config.js');
+  }
+
+  data() {
     mkdirp('data');
-    this.copy('data/widgets.json', 'data/widgets.json');
-  },
-  spec: function () {
+    this.fs.copy('data/widgets.json', 'data/widgets.json');
+  }
+
+  spec() {
     mkdirp('spec');
-    this.copy('spec/utils_spec.js', 'spec/utils_spec.js');
-    this.copy('spec/widget_spec.js', 'spec/widget_spec.js');
-  },
-  postman: function () {
+    this.fs.copy('spec/utils_spec.js', 'spec/utils_spec.js');
+    this.fs.copy('spec/widget_spec.js', 'spec/widget_spec.js');
+  }
+
+  postman() {
     mkdirp('spec');
-    this.copy('postman/localhost-restify.postman_environment.json',
+    this.fs.copy('postman/localhost-restify.postman_environment.json',
       'postman/localhost-restify.postman_environment.json');
-    this.copy('postman/Widgets.postman_collection.json',
+    this.fs.copy('postman/Widgets.postman_collection.json',
       'postman/Widgets.postman_collection.json');
-  },
-  grunt: function () {
-    this.copy('Gruntfile.js', 'Gruntfile.js');
-  },
-  projectfiles: function () {
-    this.copy('package.json', 'package.json');
-    this.copy('db-connection.js', 'db-connection.js');
-    this.copy('log.js', 'log.js');
-    this.copy('.npmignore', '.npmignore');
-    this.copy('.gitignore', '.gitignore');
+  }
+
+  grunt() {
+    this.fs.copy('Gruntfile.js', 'Gruntfile.js');
+  }
+
+  projectfiles() {
+    this.fs.copy('package.json', 'package.json');
+    this.fs.copy('db-connection.js', 'db-connection.js');
+    this.fs.copy('log.js', 'log.js');
+    this.fs.copy('.npmignore', '.npmignore');
+    this.fs.copy('.gitignore', '.gitignore');
     //npm renames .gitignore in .npmignore when publishing,
     //so rename back to .gitignore
     if (this.fs.exists(this.templatePath('.npmignore'))) {
       this.fs.copy('.npmignore', '.gitignore');
     }
-    this.copy('.editorconfig', '.editorconfig');
+    this.fs.copy('.editorconfig', '.editorconfig');
   }
-});
+};
